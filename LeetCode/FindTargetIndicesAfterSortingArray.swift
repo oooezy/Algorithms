ad4e3d3eb64bb2e10f8ec15cd2281767d16ea0c8
oooezy <leeeeunz@gmail.com>
2022-02-09 01:42:58 +0900

    [LeetCode] 2089. Find Target Indices After Sorting Array

--- LeetCode/FindTargetIndicesAfterSortingArray.swift
+++ LeetCode/FindTargetIndicesAfterSortingArray.swift
@@ -1,20 +1,22 @@
 /*
-Title     : 1920. Build Array from Permutation
-URL       : https://leetcode.com/problems/build-array-from-permutation/
+Title     : 2089. Find Target Indices After Sorting Array
+URL       : https://leetcode.com/problems/find-target-indices-after-sorting-array/
 Author    : EUNJI LEE
-Created   : 2022.01.27
+Created   : 2022.02.09
 */
 
 // Solution 1.
 class Solution {
-    func buildArray(_ nums: [Int]) -> [Int] {
-        var res = nums
+    func targetIndices(_ nums: [Int], _ target: Int) -> [Int] {
+        var sorted = nums.sorted()
+        var res: [Int] = []
         
-        for i in nums {
-            res[i] = nums[nums[i]]
+        for i in 0..<sorted.count {
+            if sorted[i] == target {
+                res.append(i)
+            }
         }
         
         return res
     }
 }
-

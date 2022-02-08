03aa6005d74ef44e12dd64a77e186c288a66aa99
oooezy <leeeeunz@gmail.com>
2022-02-09 01:42:36 +0900

    [LeetCode] 1678. Goal Parser Interpretation

--- LeetCode/GoalParserInterpretation.swift
+++ LeetCode/GoalParserInterpretation.swift
@@ -1,20 +1,30 @@
 /*
-Title     : 1920. Build Array from Permutation
-URL       : https://leetcode.com/problems/build-array-from-permutation/
+Title     : 1678. Goal Parser Interpretation
+URL       : https://leetcode.com/problems/goal-parser-interpretation/
 Author    : EUNJI LEE
-Created   : 2022.01.27
+Created   : 2022.02.09
 */
 
 // Solution 1.
 class Solution {
-    func buildArray(_ nums: [Int]) -> [Int] {
-        var res = nums
+    func interpret(_ command: String) -> String {
+        let st = Array(command)
+        var res = "", i = 0
         
-        for i in nums {
-            res[i] = nums[nums[i]]
+        while (i < st.count) {
+            if st[i] == "G" {
+                res += "G"
+                i += 1
+            } else {
+                if st[i + 1] == ")" {
+                    res += "o"
+                    i += 2
+                } else {
+                    res += "al"
+                    i += 4
+                }
+            }
         }
-        
         return res
     }
 }
-

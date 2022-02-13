f44318ddbb64f4ea12ef06206de36a80d91fb22f
oooezy <leeeeunz@gmail.com>
2022-02-13 23:41:06 +0900

    [LeetCode] 709. To Lower Case

---
+++ LeetCode/ToLowerCase.swift
@@ -0,0 +1,13 @@
+/*
+Title     : 709. To Lower Case
+URL       : https://leetcode.com/problems/to-lower-case/
+Author    : EUNJI LEE
+Created   : 2022.02.13
+*/
+
+// Solution 1.
+class Solution {
+    func toLowerCase(_ s: String) -> String {
+        return s.lowercased()
+    }
+}

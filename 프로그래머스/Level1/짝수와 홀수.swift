087691890481da25cf53bb9a6752339aabfc05a9
oooezy <leeeeunz@gmail.com>
2022-02-21 09:44:52 +0900

    [프로그래머스] 짝수와 홀수

---
+++ 프로그래머스/Level1/짝수와 홀수.swift
@@ -0,0 +1,11 @@
+/*
+Title     : 짝수와 홀수
+URL       : https://programmers.co.kr/learn/courses/30/lessons/12937
+Author    : EUNJI LEE
+Created   : 2022.02.21
+*/
+
+// Solution 1.
+func solution(_ num:Int) -> String {
+    return num % 2 == 0 ? "Even" : "Odd"
+}

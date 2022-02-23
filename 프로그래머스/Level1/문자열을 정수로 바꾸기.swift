a69e9ce4e50ad67d8ea515397d03ea635ea36508
oooezy <leeeeunz@gmail.com>
2022-02-23 14:45:29 +0900

    [프로그래머스] 문자열을 정수로 바꾸기

---
+++ 프로그래머스/Level1/문자열을 정수로 바꾸기.swift
@@ -0,0 +1,11 @@
+/*
+Title     : 문자열을 정수로 바꾸기
+URL       : https://programmers.co.kr/learn/courses/30/lessons/12925
+Author    : EUNJI LEE
+Created   : 2022.02.23
+*/
+
+// Solution 1.
+func solution(_ s:String) -> Int {
+    return Int(s)!
+}

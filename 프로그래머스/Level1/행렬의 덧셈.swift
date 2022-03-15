a4ac56c9bddf2805a8aa2d11897d880ee41f43b3
oooezy <leeeeunz@gmail.com>
2022-03-15 12:00:05 +0900

    [프로그래머스] 행렬의 덧셈 : 풀이 추가

--- 프로그래머스/Level1/행렬의 덧셈.swift
+++ 프로그래머스/Level1/행렬의 덧셈.swift
@@ -9,3 +9,16 @@ Created   : 2022.02.21
 func solution(_ arr1:[[Int]], _ arr2:[[Int]]) -> [[Int]] {
     return zip(arr1, arr2).map {zip($0, $1).map { $0 + $1 }}
 }
+
+// Solturion 2.
+func solution(_ arr1:[[Int]], _ arr2:[[Int]]) -> [[Int]] {
+    var arr3: [[Int]] = arr1
+    
+    for i in 0..<arr1.count {
+        for j in 0..<arr1[i].count {
+            arr3[i][j] = arr1[i][j] + arr2[i][j]
+        }
+    }
+    
+    return arr3
+}

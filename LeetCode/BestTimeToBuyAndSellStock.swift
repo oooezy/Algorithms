e08b8d55f550db52d24b2f8aa0fb44d3aff26053
oooezy <leeeeunz@gmail.com>
2022-05-11 23:32:53 +0900

    [LeetCode] 121. Best Time to Buy and Sell Stock

---
+++ LeetCode/BestTimeToBuyAndSellStock.swift
@@ -0,0 +1,27 @@
+/*
+Title     : 121. Best Time to Buy and Sell Stock
+URL       : https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+Author    : EUNJI LEE
+Created   : 2022.05.11
+*/
+
+// Solution 1.
+class Solution {
+    func maxProfit(_ prices: [Int]) -> Int {
+        if prices.isEmpty {
+            return 0
+        }
+
+        var min = prices[0]
+        var result = 0
+
+        for i in prices {
+            if i < min {
+                min = i
+            } else if (i - min) > result {
+                result = i - min
+            }
+        }
+        return result
+    }
+}

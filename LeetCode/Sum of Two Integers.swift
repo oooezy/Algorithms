b69fa9f0b99d90de8eb78aa5520100aefd083595
eunjileee <48424423+eunjileee@users.noreply.github.com>
2022-05-18 11:08:00 +0900

    [LeetCode] 371. Sum of Two Integers

---
+++ LeetCode/Sum of Two Integers.swift
@@ -0,0 +1,13 @@
+/*
+Title     : 371. Sum of Two Integers
+URL       : https://leetcode.com/problems/sum-of-two-integers/
+Author    : EUNJI LEE
+Created   : 2022.05.18
+*/
+
+// Solution 1.
+class Solution {
+    func getSum(_ a: Int, _ b: Int) -> Int {
+        return a.advanced(by: b)
+    }
+}

158578d6bcc70b70469e17fdd6517b21d3fca312
eunjileee <leeeeunz@gmail.com>
2022-08-26 23:34:01 +0900

    [LeetCode] 1941. Check if All Characters Have Equal Number of Occurrences

---
+++ LeetCode/Check if All Characters Have Equal Number of Occurrences.swift
@@ -0,0 +1,20 @@
+/*
+Title     : 1941. Check if All Characters Have Equal Number of Occurrences
+URL       : https://leetcode.com/problems/check-if-all-characters-have-equal-number-of-occurrences/
+Author    : EUNJI LEE
+Created   : 2022.08.26
+*/
+
+// Solution 1.
+class Solution {
+     func areOccurrencesEqual(_ s: String) -> Bool {
+        var dict = [Character:Int]()
+        var charArray = Array(s)
+         
+        for char in charArray {
+            dict[char, default: 0] += 1
+        }
+        
+        return dict.values.min() == dict.values.max()
+    }
+}

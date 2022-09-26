ced2646099064270e9f66581f68d7700ef25e9e0
eunjileee <leeeeunz@gmail.com>
2022-09-26 12:35:07 +0900

    [프로그래머스] 약수의 합 : 풀이수정

--- 프로그래머스/Level1/약수의 합.swift
+++ 프로그래머스/Level1/약수의 합.swift
@@ -7,5 +7,8 @@ Created   : 2022.02.26
 
 // Solution 1.
 func solution(_ n:Int) -> Int {
-    return Array(1...n).filter{ n % $0 == 0 }.reduce(0, +)
+    guard n != 0 else {
+        return 0
+    }
+    return Array(1...n).filter { n % $0 == 0 }.reduce(0, +)
 }
